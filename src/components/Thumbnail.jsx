42d0c40afe25374d6ff6b9c9fabdbc4351eb07e1
Brett Kuhn <brettkuhn87@gmail.com>
2023-08-20 12:20:13 -0600

    Adds basic layout for video thumbnails

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
-const Thumbnail = () => {
+const Thumbnail = ({savedSection}) => {
+
+
   return (
-    <div>
-      
+    <div className='border-2 border-teal-200 h-5/6 p-2 m-4 flex flex-col items-center justify-center'>
+      <img className='hover:scale-105 hover:cursor-pointer'/>
+      <p className='text-center'>title</p>
+      <p className='text-center'>description</p>
+      <p className='text-center'>what other info?</p>
+      {savedSection && 
+        <button 
+          type='button' 
+          className='border-1 border-black px-2 mt-2'>
+            Remove 🗑️
+        </button>
+      }
+      {!savedSection &&
+        <button 
+          type='button' 
+          className='border-1 border-black px-2 mt-2'>
+            Save ➕
+        </button>
+      }
     </div>
   )
 }
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import Thumbnail from './Thumbnail'
 
 const Videos = () => {
 
-  const currentSection = 'Videos'
+  const savedSection = false;
 
   return (
     <section className='flex flex-col items-center border-2 border-stone-300'>
       <h2 className='mt-6 text-xl md:text-3xl'>Popular Videos</h2>
-      <div className='border-2 border-red-500 w-5/6 my-10 h-64 md:h-80 flex flex-row overflow-x-auto'>
-
+      <div className='border-2 border-red-500 w-5/6 my-10 h-64 md:h-80 flex flex-row items-center overflow-x-auto'>
+        <Thumbnail savedSection={savedSection}/>
+        <Thumbnail savedSection={savedSection}/>
+        <Thumbnail savedSection={savedSection}/>
+        <Thumbnail savedSection={savedSection}/>
+        <Thumbnail savedSection={savedSection}/>
+        <Thumbnail savedSection={savedSection}/>
+        <Thumbnail savedSection={savedSection}/>
+        <Thumbnail savedSection={savedSection}/>
+        <Thumbnail savedSection={savedSection}/>
+        <Thumbnail savedSection={savedSection}/>
+        <Thumbnail savedSection={savedSection}/>
+        <Thumbnail savedSection={savedSection}/>
+        <Thumbnail savedSection={savedSection}/>
+        <Thumbnail savedSection={savedSection}/>
       </div>
     </section>
   )
