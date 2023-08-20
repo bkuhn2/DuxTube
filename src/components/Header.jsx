d54720c594afffb7433c810064a82a271e1c196f
Brett Kuhn <brettkuhn87@gmail.com>
2023-08-20 11:45:03 -0600

    Adds additional HTML elements for link and subheading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Header = () => {
   return (
     <header className='flex flex-row justify-between p-5 border-4 border-cyan-400'>
-      <h1 className='ml-4'>DuxTube</h1>
+      <div className='ml-4 flex flex-col items-start'>
+        <h1 >DuxTube</h1>
+        <p >Redux + Video Experience Project</p>
+        <p >Link to Pexels</p>
+      </div>
       <p className='mr-4'>Saved Videos: 0</p>
     </header>
   )
