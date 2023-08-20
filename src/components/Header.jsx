ab635657ccc61cb1576f89e78748ebda46743661
Brett Kuhn <brettkuhn87@gmail.com>
2023-08-20 11:24:48 -0600

    Creates basic structure for header component and imports to App

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header className='flex flex-row justify-between p-5 border-4 border-cyan-400'>
+      <h1 className='ml-4'>DuxTube</h1>
+      <p className='mr-4'>Saved Videos: 0</p>
+    </header>
   )
 }
 
