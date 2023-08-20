e0661d22cea81bdb81ecf5fec9d3b54d94121b30
Brett Kuhn <brettkuhn87@gmail.com>
2023-08-20 11:44:41 -0600

    Adds basic structure to popular videos section and imports into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Videos from './components/Videos'
 
 function App() {
 
   return (
     <main>
       <Header />
+      <Videos />
     </main>
   )
 }
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Videos = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='flex flex-col items-center border-2 border-stone-300'>
+      <h2 className='mt-6 text-xl md:text-3xl'>Popular Videos</h2>
+      <div className='border-2 border-red-500 w-5/6 my-10 h-64 md:h-80 flex flex-row overflow-x-auto'>
+
+      </div>
+    </section>
   )
 }
 
