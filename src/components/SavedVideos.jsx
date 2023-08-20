4264b934682a12ef9140ca45e5e60afe7e24019f
Brett Kuhn <brettkuhn87@gmail.com>
2023-08-20 11:54:38 -0600

    Creates basic layout for Saved Videos

--- src/components/SavedVideos.jsx
+++ src/components/SavedVideos.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 const SavedVideos = () => {
+
+  const currentSection = 'Saved Videos';
+
   return (
-    <div>
-      
-    </div>
+    <section className='flex flex-col items-center border-2 border-stone-300'>
+      <h2 className='mt-6 text-xl md:text-3xl'>My Saved Videos</h2>
+      <div className='border-2 border-red-500 w-5/6 my-10 h-64 md:h-80 flex flex-row overflow-x-auto'>
+
+      </div>
+    </section>
   )
 }
 
