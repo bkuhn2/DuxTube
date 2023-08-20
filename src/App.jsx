d9cff271dd0caab68b7f283e7a7df1d7657cc641
Brett Kuhn <brettkuhn87@gmail.com>
2023-08-20 11:53:39 -0600

    Minor style updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Videos from './components/Videos'
+import SavedVideos from './components/SavedVideos'
 
 function App() {
 
   return (
-    <main>
+    <main className='h-screen w-screen'>
       <Header />
       <Videos />
+      <SavedVideos />
     </main>
   )
 }
