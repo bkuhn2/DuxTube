02a404d5f67ea15738bfc6b029db3cadf49fe29f
Brett Kuhn <brettkuhn87@gmail.com>
2023-08-20 14:10:09 -0600

    Adds Pexels JavaScript library to get video data

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,17 @@ import './App.css'
 import Header from './components/Header'
 import Videos from './components/Videos'
 import SavedVideos from './components/SavedVideos'
+import { createClient } from 'pexels';
 
 function App() {
+
+  const client = createClient('563492ad6f917000010000013592270a849047e58b69ae21118f8b70');
+
+  client.videos.popular({ per_page: 20 })
+    .then(videos => {
+      console.log(videos.videos);
+    });
+
 
   return (
     <main className='h-screen w-screen'>
