080ac7b0f1d287793b36f471d037026d6fa52ff3
Brett Kuhn <brettkuhn87@gmail.com>
2023-08-20 16:20:11 -0600

    Updates Thumbnail styles and props to incorporate real data and images

--- src/components/SavedVideos.jsx
+++ src/components/SavedVideos.jsx
@@ -8,7 +8,7 @@ const SavedVideos = () => {
   return (
     <section className='flex flex-col items-center border-2 border-stone-300'>
       <h2 className='mt-6 text-xl md:text-3xl'>My Saved Videos</h2>
-      <div className='border-2 border-red-500 w-5/6 my-10 h-64 md:h-80 flex flex-row items-center overflow-x-auto'>
+      <div className='border-2 border-red-500 w-11/12 my-10 h-max flex flex-row overflow-x-scroll'>
         <Thumbnail savedSection={savedSection}/>
       </div>
     </section>
--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -1,28 +1,33 @@
 import React from 'react'
 
-const Thumbnail = ({savedSection}) => {
+const Thumbnail = ({savedSection, id, uploader, image}) => {
 
 
   return (
-    <div className='border-2 border-teal-200 h-5/6 p-2 m-4 flex flex-col items-center justify-center'>
-      <img className='hover:scale-105 hover:cursor-pointer'/>
-      <p className='text-center'>title</p>
-      <p className='text-center'>description</p>
-      <p className='text-center'>what other info?</p>
-      {savedSection && 
-        <button 
-          type='button' 
-          className='border-1 border-black px-2 mt-2'>
-            Remove 🗑️
-        </button>
-      }
-      {!savedSection &&
-        <button 
-          type='button' 
-          className='border-1 border-black px-2 mt-2'>
-            Save ➕
-        </button>
-      }
+    <div className='border-2 border-teal-200 p-4 m-2 flex flex-col items-center justify-between min-w-fit'>
+      <img className='hover:scale-105 hover:cursor-pointer mb-6 max-h-28 md:max-h-64 '
+        src={image}
+      />
+      <div className='flex flex-col items-center'>
+        <p className='text-center'>Uploaded by {uploader}</p>
+        {savedSection && 
+          <button 
+            type='button' 
+            className='border-1 border-black px-2 mt-2'
+            id={id}>
+              Remove 🗑️
+          </button>
+        }
+        {!savedSection &&
+          <button 
+            type='button' 
+            className='border-1 border-black px-2 mt-2'
+            id={id}>
+              Save ➕
+          </button>
+        } 
+      </div>
+
     </div>
   )
 }
